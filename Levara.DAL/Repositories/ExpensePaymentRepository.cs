d83ea3d122c78a7d3e5264b1f101e18a15e9b25c
matif <matias.fojgiel@team.wirall.com>
2024-12-27 16:51:19 -0300

    Fix ExpenseCharge and ExpensePayment

---
+++ Levara.DAL/Repositories/ExpensePaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Levara.Domain.Contexts;
+using Levara.Domain.DAL.Repositories;
+using Levara.Domain.Models;
+
+namespace Levara.DAL.Repositories;
+
+public class ExpensePaymentRepository : Repository<ExpensePayment>, IExpensePaymentRepository
+{
+    public ExpensePaymentRepository(UnitOfWork unitOfWork,
+        IUserContext userContext) : base(unitOfWork, userContext)
+    {
+
+    }
+}
