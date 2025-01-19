f74b57366cf0b236b5c4accc4316c95b898a3216
Solia Guillermo <soliaguille@gmail.com>
2025-01-19 17:14:26 -0300

    add CreateLeasePaymentCommand

---
+++ Levara.Application/Plaid/CreateMaintenancePayment/CreateMaintenancePaymentCommand.cs
@@ -0,0 +1,24 @@
+﻿using Levara.Shared.Domain.Bus.Commands;
+using System.ComponentModel.DataAnnotations;
+
+namespace Levara.Application.Plaid.CreateMaintenancePayment;
+
+public class CreateMaintenancePaymentCommand : Command<CreateMaintenancePaymentCommandResponse>
+{
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int? PlaidId {  get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int? OwnerId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int? MaintenanceChargeId { get; set; }
+
+    [Required]
+    [Range(1, Double.MaxValue)]
+    public decimal? Amount { get; set; }
+
+}
---
+++ Levara.Application/Plaid/CreateMaintenancePayment/CreateMaintenancePaymentCommandResponse.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Levara.Application.Plaid.CreateMaintenancePayment;
+
+public class CreateMaintenancePaymentCommandResponse
+{
+    public int Id {  get; set; }
+}
