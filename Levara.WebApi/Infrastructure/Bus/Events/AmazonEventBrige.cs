be6e4cd1ff12aad9948f248580f523f81b86a86f
soliaguille <68518827+soliaguille@users.noreply.github.com>
2025-06-03 19:31:15 -0300

    fix publish Amazon Event Brige: batch 10 (#14)

--- Levara.WebApi/Infrastructure/Bus/Events/AmazonEventBrige.cs
+++ Levara.WebApi/Infrastructure/Bus/Events/AmazonEventBrige.cs
@@ -8,46 +8,74 @@ namespace Levara.WebApi.Infrastructure.Bus.Events;
 public class AmazonEventBrige : IEventBus
 {
     private readonly IAmazonEventBridge _eventBridgeClient;
+    private readonly ILogger<AmazonEventBrige> _logger;
+
     private const string EventBusName = "default";
-    public AmazonEventBrige(IAmazonEventBridge eventBridgeClient)
+    public AmazonEventBrige(IAmazonEventBridge eventBridgeClient,
+        ILogger<AmazonEventBrige> logger)
     {
         _eventBridgeClient = eventBridgeClient;
+        _logger = logger;
     }
 
     public async Task PublishAsync(List<IDomainEvent> domainEvents)
     {
 
-        List<PutEventsRequestEntry> entries = new();
-        foreach (var domainEvent in domainEvents)
+        if (domainEvents == null || !domainEvents.Any())
+            return;
+
+
+        var batches = domainEvents.Select((x, i) => new { Index = i, Value = x })
+                                  .GroupBy(x => x.Index / 10)
+                                  .Select(x => x.Select(v => v.Value).ToList());
+
+        foreach (var batch in batches)
         {
-            var serializedDomainEvent = JsonConvert.SerializeObject(domainEvent);
+            await PublishBatchAsync(batch);
+        }
+    }
 
-            PutEventsRequestEntry entry = new()
+    private async Task PublishBatchAsync(List<IDomainEvent> domainEvents)
+    {
+        try
+        {
+            List<PutEventsRequestEntry> entries = new();
+            foreach (var domainEvent in domainEvents)
             {
-                EventBusName = EventBusName,
-                Source = "levara.function",
-                DetailType = domainEvent.Name,
-                Detail = serializedDomainEvent,
-                Time = DateTime.UtcNow
+                var serializedDomainEvent = JsonConvert.SerializeObject(domainEvent);
 
+                PutEventsRequestEntry entry = new()
+                {
+                    EventBusName = EventBusName,
+                    Source = "levara.function",
+                    DetailType = domainEvent.Name,
+                    Detail = serializedDomainEvent,
+                    Time = DateTime.UtcNow
+
+                };
+
+                entries.Add(entry);
+            }
+
+            var eventRequest = new PutEventsRequest
+            {
+                Entries = entries
             };
 
-            entries.Add(entry);
+            var response = await _eventBridgeClient.PutEventsAsync(eventRequest);
+
+            if (response.FailedEntryCount > 0)
+            {
+                _logger.LogError("Failed to publish some events. FailedEntryCount: {FailedEntryCount}", response.FailedEntryCount);
+                return;
+            }
+
+            _logger.LogInformation("Successfully published {EventCount} events to Amazon EventBridge.", entries.Count);
         }
-
-        var eventRequest = new PutEventsRequest
+        catch (Exception ex)
         {
-            Entries = entries
-        };
+            _logger.LogError(ex, "An error occurred while publishing events to Amazon EventBridge.");
 
-        var response = await _eventBridgeClient.PutEventsAsync(eventRequest);
-
-        if (response.FailedEntryCount > 0)
-        {
-            throw new Exception("Failed to publish some events.");
         }
-
-        Console.WriteLine("Event published successfully!");
-
     }
 }
